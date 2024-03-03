689aa7ffc034a9dbffc7fa0aef5f9ebff3f24314
Anik07Ghosh <120354990+Anik07Ghosh@users.noreply.github.com>
2024-03-03 15:53:33 +0530

    Add files via upload

---
+++ smartproject./Page_Buy Home Decor Items of Handicrafts an_da8078/h1_Home Decor - Items  Gifts.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h1_Home Decor - Items  Gifts</name>
+   <tag></tag>
+   <elementGuidId>7ee82685-fef4-48dd-bc19-41de82d7e756</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h1.woocommerce-products-header__title.page-title</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//main[@id='main']/header/h1</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+      <webElementGuid>6b824568-5d18-4944-b18e-edf56d7ce998</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>woocommerce-products-header__title page-title</value>
+      <webElementGuid>a3c3cf22-c3f5-401d-a3fb-ff6015d29e91</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Home Decor - Items &amp; Gifts</value>
+      <webElementGuid>8e473b7c-812c-444b-bd4f-4e12c804875c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main&quot;)/header[@class=&quot;woocommerce-products-header&quot;]/h1[@class=&quot;woocommerce-products-header__title page-title&quot;]</value>
+      <webElementGuid>a5928c7a-4e28-4b4a-87f5-016039fd448b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//main[@id='main']/header/h1</value>
+      <webElementGuid>3ef82584-80bf-4b68-bcd1-fc92e0847103</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h1</value>
+      <webElementGuid>bbad5072-d31d-425e-bedd-3db5afec5144</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h1[(text() = 'Home Decor - Items &amp; Gifts' or . = 'Home Decor - Items &amp; Gifts')]</value>
+      <webElementGuid>b6b34591-fc9c-4201-9513-58818798fbe6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ smartproject./Page_Buy Pure Handmade Bamboo Mudda Stool G_ea68e9/p_5 in stock.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_5 in stock</name>
+   <tag></tag>
+   <elementGuidId>bad237ca-ce9a-45a6-b168-17d1210029b6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='product-5442']/div[2]/form/div/div/div[3]/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>p.stock.in-stock</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>5a7a5ef5-0d33-4132-8274-d5f5b94d4a99</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>stock in-stock</value>
+      <webElementGuid>79d062b1-de2b-4848-b607-3c92e931ebea</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>5 in stock</value>
+      <webElementGuid>2f13888a-e126-4998-89f9-7dac68ad18f3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;product-5442&quot;)/div[@class=&quot;summary entry-summary&quot;]/form[@class=&quot;variations_form cart&quot;]/div[@class=&quot;single_variation_wrap&quot;]/div[@class=&quot;woocommerce-variation single_variation&quot;]/div[@class=&quot;woocommerce-variation-availability&quot;]/p[@class=&quot;stock in-stock&quot;]</value>
+      <webElementGuid>cbc4d2c9-c2ce-49fe-bac8-0804ea180b51</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='product-5442']/div[2]/form/div/div/div[3]/p</value>
+      <webElementGuid>84205ab7-a56d-4022-91f4-c8aeb0054db4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/p</value>
+      <webElementGuid>ad802a88-fb4a-4a4e-9184-7ecd0adf5705</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = '5 in stock' or . = '5 in stock')]</value>
+      <webElementGuid>447ee2ae-5b1e-4d77-b742-74680ac72ba3</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
