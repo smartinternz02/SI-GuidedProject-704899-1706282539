1b646a1ef42a851e5b1dc6414a90ea9a10426e05
Anik07Ghosh <120354990+Anik07Ghosh@users.noreply.github.com>
2024-03-03 13:47:14 +0530

    Add files via upload

---
+++ smartproject./Libs/CustomKeywords.groovy
@@ -0,0 +1,18 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+
+
+def static "craferia.custofunctions_craferia.CheckDropDownListElementExist"(
+    	TestObject object	
+     , 	String option	) {
+    (new craferia.custofunctions_craferia()).CheckDropDownListElementExist(
+        	object
+         , 	option)
+}
